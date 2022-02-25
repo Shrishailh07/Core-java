0d81934e4f26551556c2988909f302268d5fa875
Shrishailh07 <shrishailherkal7@gmail.com>
2022-02-25 20:21:39 +0530

    add folder : added datatypes

---
+++ datatypes/datatypes.java
@@ -0,0 +1,20 @@
+class table{
+         public static void main(String[]m){
+        byte b = 20;
+        System.out.println(b);
+        short S = 18354;
+        System.out.println(S);
+        int i = 779542542;
+        System.out.println(i);
+        long L = 7795425424l;
+         System.out.println(L);
+        float f = 0.242526f;
+          System.out.println(f);
+        double D = 0.632563573265;
+         System.out.println(D);
+        char c = 'M';
+         System.out.println(c);
+        boolean iamRight = true;
+         System.out.println(iamRight);
+  }
+}
