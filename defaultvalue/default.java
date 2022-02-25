7033b9d8a6b0004cf889230bbf2cf29cc414062f
Shrishailh07 <shrishailherkal7@gmail.com>
2022-02-25 20:26:43 +0530

    add folder : added defaultvalue

---
+++ defaultvalue/default.java
@@ -0,0 +1,23 @@
+class defaultValue{
+         
+         static byte dvByte;
+         static short dvShort;
+         static int dvInt;
+         static long dvLong;
+         static float dvFloat;
+         static double dvDouble;
+         static char dvChar;
+         static boolean dvBoolean;
+
+       public static void main(String []mj){
+
+       System.out.println(dvByte);
+       System.out.println(dvShort);
+       System.out.println(dvInt);
+       System.out.println(dvLong);
+       System.out.println(dvFloat);
+       System.out.println(dvDouble);
+       System.out.println(dvChar);
+       System.out.println(dvBoolean);
+     }
+}
