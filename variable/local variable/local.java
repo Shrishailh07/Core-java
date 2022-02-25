95a55b2f41043b7712cf4d876d8760d9baca912b
Shrishailh07 <shrishailherkal7@gmail.com>
2022-02-25 20:31:26 +0530

    add folder : added variable

---
+++ variable/local variable/local.java
@@ -0,0 +1,10 @@
+class student{
+     public static void main(String[]mj){
+     byte age;
+     age = 23;
+     short marks;
+     marks = 100;
+    System.out.println(age);
+    System.out.println(marks);
+  }
+}
---
+++ variable/var.java
@@ -0,0 +1,23 @@
+class student{
+         
+         static byte age = 23;
+         static short marks=100;
+         static int subjects = 6;
+         static long phno = 7795425424l;
+         static float cgpa = 6.35f;
+         static double height = 5.6;
+         static char division = 'A';
+         static boolean student_is_good = true;
+
+       public static void main(String []mj){
+
+       System.out.println(age);
+       System.out.println("marks="+marks);
+       System.out.println("subjects="+subjects);
+       System.out.println("phno="+phno);
+       System.out.println("cgpa="+cgpa);
+       System.out.println("height="+height);
+       System.out.println(division);
+       System.out.println("student is good="+student_is_good);
+     }
+}
