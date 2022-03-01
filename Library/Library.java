0c8683113104fa646082fd28208a54a55a0e9a4c
Shrishailh07 <shrishailherkal7@gmail.com>
2022-03-01 10:17:41 +0530

    add folder : added Library

---
+++ Library/Library.java
@@ -0,0 +1,29 @@
+public class Library 
+{
+	private static void hero(String hero)
+	{
+		System.out.println(hero);
+	}
+static void bookdirector(String director)
+{
+	System.out.println(director);
+}
+static void release(boolean Release)
+{
+	 System.out.println(Release);
+}
+ static void pages(int pages)
+ {
+	 System.out.println(pages);
+ }
+public static void book(String language,double price)
+{
+	System.out.println(language + price );
+ }
+
+public static void main(String []args) 
+{
+	hero("Yash");
+}
+
+}
---
+++ Library/Libry.java
@@ -0,0 +1,12 @@
+class Libry {
+  public static void main(String[] mj)
+  {
+	  Library.bookdirector("Prashant Neel");
+	  Library.book("Kannada " , 999.99);
+	  Library.pages(200);
+	  Library.release(true);
+  }
+  
+
+}
+
