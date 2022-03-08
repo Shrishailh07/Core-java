3f73ffb1d58344ac5d1210545fe3a167391014d5
Shrishailh07 <shrishailherkal7@gmail.com>
2022-03-08 09:02:46 +0530

    add folder : added AdharCard

---
+++ AdharCard/src/com/AdhaarTester.java
@@ -0,0 +1,31 @@
+package com;
+import com.xworkz.adhaar.dto.AdhaarCard;
+public class AdhaarTester
+{
+public static void main(String[] mj)
+{
+	AdhaarCard ad = new AdhaarCard();
+	
+	ad.setAdhaarcardno(324554649809l);
+	ad.setAge((byte) 23);
+	ad.setGender("Male");
+	
+	System.out.println(ad);
+	
+AdhaarCard ab = new AdhaarCard();
+	
+	ab.setAdhaarcardno(43564649809l);
+	ab.setAge((byte) 24);
+	ab.setGender("Female");
+	System.out.println(ab);
+	
+AdhaarCard ac = new AdhaarCard();
+	
+	ac.setAdhaarcardno(75844649809l);
+	ac.setAge((byte) 26);
+	ac.setGender("Male");
+	System.out.println(ac);
+	
+	
+}
+}
---
+++ AdharCard/src/com/xworkz/adhaar/dto/AdhaarCard.java
@@ -0,0 +1,45 @@
+package com.xworkz.adhaar.dto;
+
+public class AdhaarCard 
+{
+private long Adhaarcardno;
+private String gender;
+private byte age;
+
+public AdhaarCard()
+{
+}
+public long getAdhaarcardno() {
+	return Adhaarcardno;
+}
+
+public void setAdhaarcardno(long adhaarcardno) {
+	Adhaarcardno = adhaarcardno;
+}
+
+public String getGender() {
+	return gender;
+}
+
+public void setGender(String gender) {
+	this.gender = gender;
+}
+
+public byte getAge() {
+	return age;
+}
+
+public void setAge(byte age) {
+	this.age = age;
+}
+
+
+
+@Override
+public String toString()
+{
+	return "AdhaarCard - [no = "+Adhaarcardno+" ,"+"Gender="+gender+" ,"+"Age ="+age+"]";
+  			
+
+}
+}
